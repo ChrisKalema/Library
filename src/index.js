6acfa28be1d8bb975b4beeb6ef718c09c2a6bf90
Chris Kalema <chriskalma01@gmail.com>
2020-05-09 16:11:55 +0300

    modification

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import './test.css';
+import Tooltip from './lib-ui/tooltip';
+import Dropdown from './lib-ui/dropdown.js';
+import Tabs from './lib-ui/tabs.js';
 
-console.log('Hello darkness my old friend');
+//create tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+tooltip.init();
+
+//create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown =>{
+    const instance = new Dropdown(dropdown);
+    instance.init();
+})
+//create tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
